5e9baaf854e95de9eaa0f9d8ad004312d4e91258
dylanlo8 <lojingjiedylan@gmail.com>
2023-06-10 00:19:48 +0800

    First Commit

---
+++ Processor.py
@@ -0,0 +1,37 @@
+import os
+from DataSynthesizer.DataDescriber import DataDescriber
+
+class SDVProcessor:
+    def __init__(self, data, param_dict):
+        params_required = []
+        self.data = data
+        
+        for param in param_dict.keys:
+            if param not in params_required:
+                raise ValueError("Unspecified Parameter")
+            
+        self.param_dict = param_dict
+          
+    def process(self):
+       pass
+    
+class DataSynthesizerProcessor:
+    description_file = os.getcwd() + '/workingfolder/description.json'
+    
+    def __init__(self, data, param_dict):
+        params_required = ["categorical_attributes", "epsilon", "degree_of_bayesian_network", "num_tuples_to_generate"]
+        self.data = data
+        
+        for param in param_dict.keys:
+            if param not in params_required:
+                raise ValueError("Unspecified Parameter, Please follow the correct naming convention")
+            
+        self.param_dict = param_dict
+           
+    def process(self):
+        describer = DataDescriber()
+        describer.describe_dataset_in_correlated_attribute_mode(dataset_file = self.data, 
+                                                                epsilon = self.param_dict["epsilon"], 
+                                                                k= self.param_dict["degree_of_bayesian_network"],
+                                                                attribute_to_is_categorical = self.param_dict["categorical_attributes"])
+        describer.save_dataset_description_to_file(self.description_file)
---
+++ SynPiper.py
@@ -0,0 +1,51 @@
+from Processor import SDVProcessor, DataSynthesizerProcessor
+from DataSynthesizer.DataGenerator import DataGenerator
+import os
+
+class SynPiper:
+    synthetic_filepath = os.getcwd + '/synthetic.csv'
+    
+    #synthesizer_name : name of synthesizer 
+    #data : input data to train synthetic data generator
+    
+    ## Initialise appropriate Pre-Processors, the pre-processors will check if a correct param_dict has been specified.
+    def __init__(self, data, synthesizer_name, param_dict):
+        # SDV Pre-Processor
+        if synthesizer_name == "ctgan" or synthesizer_name == "tvae":
+            print("Initialising SDV Processor")
+            self.processor = SDVProcessor(data, param_dict)
+        
+        # DataSynthesizer Pre-processor
+        elif synthesizer_name == "dpsynthesizer":
+            print("Initialising DataSynthesizer Processor")
+            self.processor = DataSynthesizerProcessor(data, param_dict)
+            
+        # Unspecified Synthesizer Name Error    
+        else:
+            raise ValueError("Unspecified Synthesizer Name inputted.")
+        
+        self.synthesizer_name = synthesizer_name
+        self.data = data
+    
+    def generate(self):
+        pass
+    
+    def generate_ctgan(self):
+        pass
+        
+        # Saves the Synthetic Data csv file to the designated filepath
+        
+    def generate_tvae(self):
+        pass
+    
+        # Saves the Synthetic Data csv file to the designated filepath
+    
+    def generate_dpsynthesizer(self, num_tuples_to_generate):
+        description_file = self.processor.process()
+        generator = DataGenerator()
+        generator.generate_dataset_in_correlated_attribute_mode(num_tuples_to_generate, description_file)
+        
+        # Saves the generated synthetic data to designated filepath
+        generator.save_synthetic_data(self.synthetic_filepath) # Saves the Synthetic Data csv file to the designated filepath
+    
+    
