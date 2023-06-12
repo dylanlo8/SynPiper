5974391fd2a33bd1eb8d7444814c8dfa434f376d
dylanlo8 <lojingjiedylan@gmail.com>
2023-06-12 10:08:26 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,11 @@
+from Processor import SDVProcessor, DataSynthesizerProcessor
+from SynPiper import SynPiper
+import pandas as pd
+import os
+
+data_file = os.path.join(os.getcwd(), "datasets", "heartprocessed.csv")
+pd.read_csv(data_file)
+
+
+
+piper = SynPiper()
