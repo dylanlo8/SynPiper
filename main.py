480b2e2b4197d6d5294082af264ffde8bcc26014
dylanlo8 <lojingjiedylan@gmail.com>
2023-06-12 10:50:39 +0800

    Checkpoint 1 : DataSynthesizer

--- main.py
+++ main.py
@@ -6,10 +6,18 @@ if __name__ == '__main__':
 
     data_path = os.path.join(os.getcwd(), "datasets", "heartprocessed.csv")
 
-    params_required = {"categorical_attributes" : {'sex': True, 'cp': True, 'fbs': True, 'restecg': True, 
-                                                'exang': True, 'slope': True, 'ca' : True, 'thal' : True, 'target' : True},
+    # User Input 1: Name of Synthesizer
+    synthesizer_name = "dpsynthesizer"
+
+    # User Input 2: Parameter Dictionary
+    params_required = {"categorical_attributes" : {'sex': True, 'cp': True, 'fbs': True, 
+                                                   'restecg': True, 
+                                                'exang': True, 'slope': True, 
+                                                'ca' : True, 'thal' : True, 'target' : True},
                     "epsilon" : 0,
                     "degree_of_bayesian_network" : 3}
 
-    piper = SynPiper(data_path, param_dict= params_required, synthesizer_name = "dpsynthesizer")
+    piper = SynPiper(data_path, param_dict= params_required, synthesizer_name = synthesizer_name)
+
+    # User Input 3: Number of Rows to be generated in Synthetic Dataset
     piper.generate_dpsynthesizer(num_tuples_to_generate= 297)
