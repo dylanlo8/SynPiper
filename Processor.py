3a307a7a0db7574f8959364da674995aa3e70b70
dylanlo8 <lojingjiedylan@gmail.com>
2023-06-12 14:43:03 +0800

    Added some evaluation Metrics

--- Processor.py
+++ Processor.py
@@ -6,7 +6,7 @@ class SDVProcessor:
         params_required = []
         self.data = data
         
-        for param in param_dict.keys:
+        for param in param_dict.keys():
             if param not in params_required:
                 raise ValueError("Unspecified Parameter")
             
@@ -19,23 +19,26 @@ class DataSynthesizerProcessor:
     description_file = os.path.join(os.getcwd(), 'description.json')
     
     def __init__(self, data_path, param_dict):
+        # List of Required Parameters for DataSynthesizer Model
         params_required = ["categorical_attributes", 
                            "epsilon", 
                            "degree_of_bayesian_network"]
-        self.data = data_path
         
+        # Checks if User has keyed in an appropriate Parameter Dictionary
         for param in param_dict.keys():
             if param not in params_required:
                 raise ValueError("Unspecified Parameter, Please follow the correct naming convention")
             
+        self.data_path = data_path
         self.param_dict = param_dict
            
     def process(self):
         describer = DataDescriber()
-        describer.describe_dataset_in_correlated_attribute_mode(dataset_file = self.data, 
+        describer.describe_dataset_in_correlated_attribute_mode(dataset_file = self.data_path, 
                                                                 epsilon = self.param_dict["epsilon"], 
                                                                 k= self.param_dict["degree_of_bayesian_network"],
                                                                 attribute_to_is_categorical = self.param_dict["categorical_attributes"])
+        
         print("Saving Dataset Description File")
         describer.save_dataset_description_to_file(self.description_file)
         return self.description_file
---
+++ Report.py
@@ -0,0 +1,85 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+from sklearn.metrics import normalized_mutual_info_score
+
+
+# Getting all the KS-Statistic Scores (works only for numerical columns)
+def get_all_ks_scores(real_table, synthetic_table, numerical_columns):
+  results = []
+  series_results = {}
+  for num_col in numerical_columns:
+    ks_score = KSComplement.compute(
+      real_data=real_table[num_col],
+      synthetic_data=synthetic_table[num_col]
+    )
+
+    results.append(ks_score)
+    series_results[num_col] = ks_score
+  
+  plt.figure(figsize = (15,6))
+  plots = plt.bar(numerical_columns, results)
+  plt.title("Kolmogorov-Smirnov statistic Scores")
+  print(pd.Series(series_results))
+
+#get_all_ks_scores(df, synthetic_data, numerical_columns)
+
+# Total Variational Difference (For Categorical Columns comparison)
+def get_all_variational_differences(real_table, synthetic_table, categorical_columns):
+  from sdmetrics.single_column import TVComplement
+
+  results = []
+  series_results = {}
+
+  for category in categorical_columns:
+    tv_score = TVComplement.compute(
+        real_data = real_table[category],
+        synthetic_data = synthetic_table[category])
+    results.append(tv_score)
+    series_results[category] = tv_score
+  
+  plt.figure(figsize = (15,6))
+  plots = plt.bar(categorical_columns, results)
+  plt.title("Total Variational Difference Scores")
+  print(pd.Series(series_results))
+
+def plot_corr_matrix(real, synthetic):
+    fig, (ax1, ax2) = plt.subplots(figsize=(15,6), ncols=2)
+    plt.suptitle("Pairwise Correlation Score", fontsize = 25)
+    sns.heatmap(real.corr(), ax = ax1)
+    ax1.title.set_text("Ground Truth Correlation Matrix")
+    sns.heatmap(synthetic.corr(), ax = ax2)
+    ax2.title.set_text("Synthetic Data Correlation Matrix")
+
+
+def plot_mi_matrix(df, df_syn):
+    matMI = pd.DataFrame(index = df.columns, columns = df.columns, dtype=float)
+    matMI_syn = pd.DataFrame(index = df.columns, columns = df.columns, dtype=float)
+
+
+    for row in df.columns:
+        for col in df.columns:
+            matMI.loc[row, col] = normalized_mutual_info_score(df[row],
+                                                        df[col],
+                                                        average_method = 'arithmetic')
+            matMI_syn.loc[row, col] = normalized_mutual_info_score(df_syn[row],
+                                                        df_syn[col],
+                                                        average_method = 'arithmetic')
+    
+    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(15, 6))
+    plt.suptitle("Pairwise Mutual Information Score (Normalised)", fontsize = 25)
+
+    #Real
+    sns.heatmap(matMI, 
+                cmap = sns.color_palette("ch:start=.2,rot=-.3", as_cmap=True),
+                ax = ax1)
+    ax1.set_title('Ground Truth, max=1', fontsize=15)
+            
+    
+    #Synthetic
+    sns.heatmap(matMI_syn, 
+                cmap = sns.color_palette("ch:start=.2,rot=-.3", as_cmap=True),
+                ax = ax2)
+    ax2.set_title('Synthetic Data, max=1', fontsize=15)
+    
+    plt.tight_layout()
