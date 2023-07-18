07d33131e818d79596534158af3b62ff1c7786ba
dylanlo8 <lojingjiedylan@gmail.com>
2023-07-18 17:13:56 +0800

    removed DataAnonymizer

--- SynPiper.py
+++ SynPiper.py
@@ -3,7 +3,7 @@ from DataSynthesizer.DataGenerator import DataGenerator
 import os
 from sdv.single_table import CTGANSynthesizer, TVAESynthesizer
 import pandas as pd
-from timer import Timer
+import time
 
 class SynPiper:
     """
@@ -63,11 +63,8 @@ class SynPiper:
         timer = Timer()
         timer.start()
 
-        if self.synthesizer_name == "ctgan":
+        if self.synthesizer_name == "ctgan" or self.synthesizer_name == "tvae":
             self.generate_sdv(num_tuples_to_generate=num_tuples_to_generate)
-
-        elif self.synthesizer_name == "tvae":
-            self.generate_sdv(num_tuples_to_generate= num_tuples_to_generate)
 
         elif self.synthesizer_name == "dpsynthesizer":
             self.generate_dpsynthesizer(num_tuples_to_generate=num_tuples_to_generate)
@@ -127,3 +124,28 @@ class SynPiper:
 
         # Store the synthetic samples as an attribute
         self.generated_samples = pd.read_csv(self.synthetic_filepath)
+
+
+class Timer:
+    def __init__(self):
+        self._start_time = None
+
+    def start(self):
+        """Start a new timer"""
+        if self._start_time is not None:
+            raise TimerError(f"Timer is running. Use .stop() to stop it")
+
+        self._start_time = time.perf_counter()
+
+    def stop(self):
+        """Stop the timer, and report the elapsed time"""
+        if self._start_time is None:
+            raise TimerError(f"Timer is not running. Use .start() to start it")
+
+        elapsed_time = time.perf_counter() - self._start_time
+        self._start_time = None
+        
+        return elapsed_time
+    
+class TimerError(Exception):
+    """A custom exception used to report errors in use of Timer class"""
